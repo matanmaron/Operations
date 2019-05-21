66cb9a22d86e0065426f6a63ec7e2c70a98dab3e
matanmaron <matanmaron@gmail.com>
2019-05-22 00:57:53 +0300

    * index.html: update to xamarin
    * index.js:
    * package.json:
    * package-lock.json:

--- index.js
+++
@@ -1,9 +0,0 @@
-const {app, BrowserWindow} = require('electron');
-const url = require('url');
-
-function boot(){
-    win = new BrowserWindow()
-    win.loadURL('file://' + __dirname + '/index.html');
-}
-
-app.on('ready', boot);
